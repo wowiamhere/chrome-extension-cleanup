f862c1ca49088ea438446eb8c43045977cf8bfed
wowiamhere <wowiamhere@gmail.com>
2023-03-18 22:28:08 -0700

    refracted css, added stylesheets, color input

--- js/background_color.js
+++ js/background_color.js
@@ -1,58 +1,85 @@
+extension_color_stylesheet = new CSSStyleSheet();
+document.adoptedStyleSheets = [ extension_color_stylesheet ];
+
+el_name = 'div';
+el_id = 'backgroundColor_extension';
+el = document.createElement( el_name );
+el.setAttribute('id', el_id );
+css_array = ['z-index:999999', 'width:10%', 'position:fixed', 'float:right', 'left:88%', 'height:5vw', 'border:0.5vw solid red', 'border-radius:2vw'];
+
+extension_color_stylesheet.insertRule( el.tagName + '#' + el.id + ' {' + css_array.join(';') + '}' );
+
+col_el_name = 'input';
+col_el_type = 'color';
+col_el_value = '#ababab';
+col_el_id = 'color';
+col_el = document.createElement( col_el_name );
+col_el.setAttribute('type', col_el_type );
+col_el.setAttribute('value', col_el_value );
+col_el.setAttribute('id', col_el_id );
+css_array = ['position:relative', 'display:block', 'width:80%', 'border-radius:0.5vw', 'margin:auto', 'top:0.1vw', 'height:2vw'];
+extension_color_stylesheet.insertRule( col_el.tagName + '#' + col_el.id + ' {' + css_array.join(';') + '}' );
+col_el.addEventListener('input', get_color, {once:false});
+el.appendChild( col_el );
+
 bg_color = 'background-color: rgb(171,171,171)';
+
+close_btn_name = 'div';
+close_btn_id = 'to_close';
+close_btn = document.createElement( close_btn_name );
+close_btn.setAttribute('id', close_btn_id );
+css_array = ['width:29%', 'position:relative', 'display:block', 'margin:auto', 'margin-top:0.4vw', 'background-color:red', 'text-align:center'];
+extension_color_stylesheet.insertRule( close_btn.tagName + '#' + close_btn.id + ' {' + css_array.join(';') + '}' );
+close_btn.appendChild( document.createTextNode('DONE') );
+close_btn.addEventListener('click', close_extension, {once:true});
+
+el.appendChild( close_btn );
+document.body.insertBefore(el, document.body.children[0] );
 
 handle_links('kill');
 
 document.body.addEventListener("click", body_listener, {once:false} );
 
-el = document.createElement('div');
-el.appendChild( document.createTextNode('CLOSE EXTENSION') );
-el.setAttribute('id', 'backgroundColor_extension');
-el.setAttribute('style', 'background-color: red;z-index:999999;display:block;position:sticky;top:0px;text-align:center;font-size:1.6vw;font-weight:bold');
-el.addEventListener('click', close_extension, {once:true});
-
-document.body.insertBefore(el, document.body.children[0] );
+function get_color(ev){
+  if(ev){
+    bg_color = 'background-color:' + ev.target.value + ';';
+  }else
+    return undefined
+}
 
 function filter_tags(el){
-	if (el.tagName.toLowerCase() == 'script')
-		return false;
-	else if (el.tagName.toLowerCase() == 'noscript')
-		return false;
-	else if (el.tagName.toLowerCase() == 'a')
-		return false;
-	else
-		return true;
+  if (el.tagName.toLowerCase() == 'script')
+    return false;
+  else if (el.tagName.toLowerCase() == 'noscript')
+    return false;
+  else if (el.tagName.toLowerCase() == 'a')
+    return false;
+  else
+    return true;
 }
 
 function handle_links(to_do){
-	let links = document.links;
-
-	if(to_do == 'kill'){
-		for(let i=0; i<links.length; i++){                                   	
-			let style;
-			style = links[i].style.cssText ? links[i].style.cssText : '';
-			style = style + ';pointer-events:none';
-			links[i].setAttribute('style', style);
-		}
-	}else{
-		for(let i=0; i<links.length; i++){                                   	
-                	let style;
-	              	style = links[i].style.cssText ? links[i].style.cssText : '';
-                	style = style.replace('pointer-events:none', '');
-                	style = style.replace('pointer-events: none', '');
-			links[i].setAttribute('style', style);
-		}
-		document.body.removeEventListener('click', body_listener);
-		document.getElementById('backgroundColor_extension').remove();
-	}
+  if(to_do == 'kill')
+    extension_color_stylesheet.insertRule('a{pointer-events:none !important;}');
+  else{
+    extension_color_stylesheet.removeRule(0);
+    document.body.removeEventListener('click', body_listener);
+    document.getElementById('backgroundColor_extension').remove();
+  }
 }
 
 function body_listener(ev){
-	if(ev)
-		ev.target.setAttribute('style', 'background-color: rgb(171,171,171)');
-	else
-		return undefined;
+  if(ev)
+    if(ev.target != document.getElementById('backgroundColor_extension') )
+      if(ev.target != document.getElementById('backgroundColor_extension').children[0]){
+        let style = ev.target.style.cssText;
+        style = style + bg_color;
+        ev.target.setAttribute('style', style );
+      }
+  else
+    return undefined;
 }
 
 function close_extension(){
-	handle_links('open_links');
+  handle_links('open_links');
 }
